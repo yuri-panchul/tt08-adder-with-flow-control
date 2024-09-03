170cd0f7026629cbd8c09460e2a8449d80530118
Yuri Panchul <yuri@panchul.com>
2024-09-02 18:53:00 -0700

    Undo the previous change

--- test/test.py
+++ test/test.py
@@ -38,4 +38,3 @@ async def test_project(dut):
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
-    await ClockCycles(dut.clk, 100)
